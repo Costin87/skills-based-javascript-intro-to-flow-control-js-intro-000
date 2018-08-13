baa060e4d399bfb3652da8ac187b28fe5bf0ebf7
Joe Costin <jmcostin@googlemail.com>
2018-08-13 11:13:40 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -21,11 +21,11 @@ function ageChecker(age) {
     return "You are a teenager!"
   }
   else if (true) {
-    
+
   }(age<=12){
     return "You are a kid"
   }
-  else{ 
+  else {
     return "You are a grownup"
   }
 }
